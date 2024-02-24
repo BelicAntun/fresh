b886581d4cf305b2f443a9506f06bd81ea9eeda5
Antun Belic <antun.belic@init.hr>
2024-02-24 11:25:28 +0100

    (initial): change

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   description:
     "Fresh kemijsko čišćenje - Vaš pouzdani partner za profesionalne usluge čišćenja tepiha. Specijalizirani smo za osvježavanje vaših tepiha, ostavljajući ih svježima i čistima. Kontaktirajte nas danas za visokokvalitetna rješenja čišćenja tepiha.",
   keywords:
-    "Pranje tepiha, pranje auta, pranje nameštaja, pranje stolica, pranje sedišta, kemijsko čišćenje tepiha, kemijsko čišćenje nameštaja, kemijsko čišćenje stolica, kemijsko čišćenje sjedišta, dubinsko pranje tepiha, dubinsko pranje namještaja, dubinsko pranje stolica, dubinsko pranje sjedišta, dubinsko kemijsko čišćenje tepiha, dubinsko kemijsko čišćenje nameštaja, dubinsko kemijsko čišćenje stolica, dubinsko kemijsko čišćenje sedišta, pranje tepiha Zagreb, pranje tepiha Novi Zagreb",
+    "Pranje tepiha, pranje auta, pranje nameštaja, pranje stolica, pranje sedišta, kemijsko čišćenje tepiha, kemijsko čišćenje nameštaja, kemijsko čišćenje stolica, kemijsko čišćenje sjedišta, dubinsko pranje tepiha, dubinsko pranje namještaja, dubinsko pranje stolica, dubinsko pranje sjedišta, dubinsko kemijsko čišćenje tepiha, dubinsko kemijsko čišćenje nameštaja, dubinsko kemijsko čišćenje sedišta, pranje tepiha Zagreb, pranje tepiha Novi Zagreb",
   authors: [{ name: "Fresh kemijsko čišćenje" }],
 };
 
